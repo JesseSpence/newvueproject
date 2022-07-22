1c0ec4d5778abd4b58b3b69afa5d62aab10dbbbd
Adam Castle <jesse.2601@icloud.comm>
2022-07-22 06:54:41 +0200

    firsts

---
+++ src/components/Carousel.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="Carousel">
+<slot></slot>
+
+<button @click="nex" class="nex">Next</button>
+
+<button @click="prev" class="prev">Prev</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+
+    },
+    methods: {
+        nex() {
+        this.$emit('nex')
+        },
+     prev() {
+        this.$emit('prev')
+    }
+}
+}
+
+</script>
+
+<style scoped>
+.Carousel{
+    position: relative;
+    width: 600px;
+    height:350px;
+    overflow:hidden;
+}
+button {
+    position:absolute;
+    height: 40px;
+    width: 50px;
+    top: calc(50% - 0px);
+    background-color:rgba(0, 0, 0, 0.822);
+    color:white;
+    border:none;
+}
+button:focus, button:hover {
+    outline:none;
+    cursor:pointer;
+}
+.nex {
+    right:0
+}
+.prev {
+    left:0
+}
+</style>
---
+++ src/views/Testimonials.vue
@@ -0,0 +1,69 @@
+<template>
+<div class="Slider">
+	<carousel
+	@nex="nex"
+	@prev="prev"
+	>
+	<carousel-slide v-for="(slide,index) in slides" :key="slide" :index="index" :visibleSlide="visibleSlide"> 
+	<img :src="slide.img"/>
+	<h5>{{ slide.title }}</h5>
+	</carousel-slide>
+	</carousel>
+</div>
+	
+</template>
+
+<script>
+
+import Carousel from "@/components/Carousel.vue";
+import CarouselSlide from "@/components/CarouselSlide.vue";
+export default {
+	components: {
+		Carousel ,
+			CarouselSlide
+		},
+		data() {
+			return {
+				slides: [
+					{ 'img': 'https://picsum.photos/id/237/600/350',
+					'title':'broski'},
+					{ 'img': 'https://picsum.photos/id/232/600/350' },
+					{ 'img': 'https://picsum.photos/id/263/600/350' },
+					{ 'img': 'https://picsum.photos/id/234/600/350' },
+					{ 'img': 'https://picsum.photos/id/235/600/350' },
+					{ 'img': 'https://picsum.photos/id/239/600/350' }
+				],
+				visibleSlide : 0,
+			};
+		},
+		computed: {
+		slideslen(){
+				return this.slides.length;
+			}
+		},
+		methods: {
+			nex() {
+				if(this.visibleSlide >= this.slideslen - 1) {
+					this.visibleSlide = 0;
+				}else {
+					this.visibleSlide++;
+				}
+			},
+			prev() {
+				if (this.visibleSlide <= 0) {
+					this.visibleSlide = this.slideslen - 1
+				} else {
+					this.visibleSlide--;
+				}
+			}
+
+	},
+	};
+</script>
+
+<style scoped>
+.Slider{
+	display:flex;
+	justify-content: center;
+}
+	</style>
